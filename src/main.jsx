305ce1f1dd89354b952309775dc30115759f2cac
Thiago Neves Almeida <thiagonevesdealmeida@gmail.com>
2023-05-23 15:31:37 +0100

    Add index.html and main.jsx files

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+//global css
+
+
+// importing App
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        
+    </React.StrictMode>
+)
