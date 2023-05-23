8a3bd963a335bb6874791f029c2a5b17553869df
Thiago Neves Almeida <thiagonevesdealmeida@gmail.com>
2023-05-23 15:53:55 +0100

    Import to main.jsx Home page content

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 //global css
+import './styles/global.css'
 
+//importing Home/app page
+import { Home } from './pages/Home'
 
 // importing App
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        
+
     </React.StrictMode>
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+        <div>
+            <h1>Tech Talks</h1>
+        </div>
+    )
+    
+}
